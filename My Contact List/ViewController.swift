846c03a788960d75c621b5539f0120241a647ed7
djackson136 <djackson136@student.gsu.edu>
2021-04-07 09:10:17 -0400

    Initial Commit

---
+++ My Contact List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My Contact List
+//
+//  Created by Dannah Jackson on 4/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
